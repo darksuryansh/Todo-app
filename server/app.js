527760b9bc366566a2d5ced576c91728b19bd36d
darksuryansh <suryanshbhardwaj04@gmail.com>
2025-12-23 21:12:30 +0530

    make  Dockerfile

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.js";
 import todoRouters from "./routes/todo.js";
 import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 dotenv.config();  
 connectDB();
@@ -15,6 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/users", userRoutes);
 app.use("/api/todo", todoRouters);
